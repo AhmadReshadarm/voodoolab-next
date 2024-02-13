2685af57cff1ea7b941848c407f5cf1f6e674bd9
ARM <armreshad@gmail.com>
2024-02-13 15:25:35 +0300

    changed route to /code

--- components/admin/login/helpers.ts
+++ components/admin/login/helpers.ts
@@ -1,13 +1,14 @@
-import { navigateTo } from "common/helpers";
-import { NextRouter } from "next/router";
-import { signin } from "redux/slicers/authSlicer";
-import { AppDispatch } from "redux/store";
-import { Page } from "routes/constants";
+import { navigateTo } from 'common/helpers';
+import { NextRouter } from 'next/router';
+import { signin } from 'redux/slicers/authSlicer';
+import { AppDispatch } from 'redux/store';
+import { Page } from 'routes/constants';
 
-export const handleFormSubmit = (router: NextRouter, dispatch: AppDispatch) => async (form) => {
-  const res: any = await dispatch(signin(form));
+export const handleFormSubmit =
+  (router: NextRouter, dispatch: AppDispatch) => async (form) => {
+    const res: any = await dispatch(signin(form));
 
-  if (!res.error) {
-    navigateTo(router, Page.ADMIN_CATEGORIES)();
-  }
-};
+    if (!res.error) {
+      navigateTo(router, Page.ADMIN_CODE)();
+    }
+  };
